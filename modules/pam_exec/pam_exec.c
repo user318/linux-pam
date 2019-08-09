5678c9427d15287e780d9331ff2308b112bc2f70
Alexander Zubkov <green@qrator.net>
2019-08-09 12:08:23 +0200

    pam_exec: add interactive mode Add interactive mode to pam_exec module. When options interactive and stdout are set, then program can request prompt from user by seding a string beginning with "<<" to the stdout. Module will use the rest of the string as a prompt while asking for a response from the user. Program will receive the response in its stdin. Program can request response from a user serveral times.

--- modules/pam_exec/pam_exec.c
+++ modules/pam_exec/pam_exec.c
@@ -100,6 +100,7 @@ call_exec (const char *pam_type, pam_handle_t *pamh,
   int quiet = 0;
   int expose_authtok = 0;
   int use_stdout = 0;
+  int use_interactive = 0;
   int pipe_stdin = 0;
   int optargc;
   const char *logfile = NULL;
@@ -126,6 +127,8 @@ call_exec (const char *pam_type, pam_handle_t *pamh,
 	debug = 1;
       else if (strcasecmp (argv[optargc], "stdout") == 0)
 	use_stdout = 1;
+      else if (strcasecmp (argv[optargc], "interactive") == 0)
+	use_interactive = 1;
       else if (strncasecmp (argv[optargc], "log=", 4) == 0)
 	logfile = &argv[optargc][4];
       else if (strncasecmp (argv[optargc], "type=", 5) == 0)
@@ -152,6 +155,29 @@ call_exec (const char *pam_type, pam_handle_t *pamh,
             retval = PAM_INCOMPLETE;
           return retval;
         }
+    }
+
+   if (use_interactive)
+    {
+      if (strcmp (pam_type, "auth") != 0)
+        {
+          pam_syslog (pamh, LOG_ERR,
+                      "interactive not supported for type %s", pam_type);
+          use_interactive = 0;
+        }
+      else if (expose_authtok)
+        {
+          pam_syslog (pamh, LOG_ERR,
+                      "interactive is not compatible with expose_authtok");
+          use_interactive = 0;
+        }
+      else if (!use_stdout)
+        {
+          pam_syslog (pamh, LOG_ERR, "interactive requires stdout mode");
+          use_interactive = 0;
+        }
+      else
+        pipe_stdin = 1;
     }
 
   if (expose_authtok == 1)
@@ -269,8 +295,60 @@ call_exec (const char *pam_type, pam_handle_t *pamh,
 	    {
 	      size_t len;
 	      len = strlen(buf);
-	      if (buf[len-1] == '\n')
-		buf[len-1] = '\0';
+
+	      if (buf[len-1] != '\n')
+                {
+                  pam_syslog (pamh, LOG_ERR, "message from child is too long");
+                  was_error = PAM_SYSTEM_ERR;
+                  goto finish;
+                }
+
+              buf[len-1] = '\0';
+
+              /* if string begins with "<<" - child asks for prompt */
+              if (use_interactive && buf[0] == '<' && buf[1] == '<')
+                {
+                  char *resp = NULL;
+
+                  if (debug)
+                    pam_syslog (pamh, LOG_DEBUG, "child asked for prompt");
+
+                  retval = pam_prompt (pamh, PAM_PROMPT_ECHO_OFF,
+                                       &resp, "%s", buf+2);
+
+                  if (retval != PAM_SUCCESS)
+                    {
+                      _pam_drop (resp);
+                      if (retval == PAM_CONV_AGAIN)
+                        was_error = PAM_PERM_DENIED;
+                      else
+                        was_error = retval;
+                      goto finish;
+                    }
+
+                  if (!resp)
+                    {
+                      was_error = PAM_PERM_DENIED;
+                      goto finish;
+                    }
+
+                  if (debug)
+                    pam_syslog (pamh, LOG_DEBUG, "send response to child: %s", resp);
+
+                  if (write(fds[1], resp, strlen(resp)) == -1 ||
+                      write(fds[1], "\n", 1) == -1)
+                    {
+                      _pam_drop (resp);
+                      pam_syslog (pamh, LOG_ERR,
+                                  "sending response to child failed: %m");
+                      was_error = PAM_SYSTEM_ERR;
+                      goto finish;
+                    }
+
+                  _pam_drop (resp);
+                  continue;
+                }
+
 	      pam_info(pamh, "%s", buf);
 	    }
 	}
